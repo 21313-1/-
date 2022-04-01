3cf9117646efa2bbfb8040fc42f5cb0142c59ab5
User <cjcjfjkf@gmail.com>
2022-04-01 20:25:10 +0900

    sadf

--- .vscode/게임만들기/8프레임.py
+++ .vscode/게임만들기/8프레임.py
@@ -8,7 +8,7 @@ pygame.init() #기본 초기화(필수)
 #화면 크기 설정
 screen_width = 480 #가로 크기
 screen_height = 640#세로 크기
-screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 #화면 타이틀 설정
 pygame.display.set_caption("Nado Game") #게임 이름
